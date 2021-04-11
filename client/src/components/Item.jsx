43672e08a9ff58f6b6b0e37a87f8ff10165a6add
Zhang Yunjie <e0421414@u.nus.edu>
2021-04-11 21:52:42 +0800

    Configure item. Add Window component.

--- client/src/components/Item.jsx
+++ client/src/components/Item.jsx
@@ -23,11 +23,17 @@ const Item = ({ item, index, moveItem, status}) => {
             const hoverMiddleY = (hoveredRect.bottom - hoveredRect.top) / 2;
             const mousePosition = monitor.getClientOffset();
             const hoverClientY = mousePosition.y - hoveredRect.top;
-
+            
+            // Only perform the move when the mouse has crossed half of the items height
+            // When dragging downwards, only move when the cursor is below 50%
+            // When dragging upwards, only move when the cursor is above 50%
+            
+            // Dragging downwards
             if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
                 return;
             }
 
+            // Dragging upwards
             if (dragIndex > hoverIndex && hoverClientY < hoverMiddleY) {
                 return;
             }
@@ -37,6 +43,39 @@ const Item = ({ item, index, moveItem, status}) => {
         }
     });
 
+    const [{ isDragging }, drag] = useDrag({
+        item: {type: ITEM_TYPE, ...item, index},
+        collect: monitor => ({
+            isDragging: monitor.isDragging()
+        })
+    });
 
+    const [show, setShow] = useState(false);
+    
+    const onOpen = () => setShow(true);
+
+    const onClose = () => setShow(false);
+
+    drag(drop(ref));
+
+    return (
+        <Fragment>
+            <div
+                ref = {ref}
+                style = {{ opacity: isDragging ? 0: 1}}
+                className = {"item"}
+                onClick = {onOpen}
+            >
+                <div className = {"color-bar"} style = {{backgroundcolor: status.color}} />
+                <p className={"item-title"}>{item.content}</p>
+                <p className={"item-status"}>{item.icon}</p>
+            </div>
+            <Window
+                item = {item}
+                onClose = {onClose}
+                show = {show}
+            />
+        </Fragment>
+    )
 
 }
