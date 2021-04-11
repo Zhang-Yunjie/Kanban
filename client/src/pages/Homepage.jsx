b65581c53b42e58e6e90e8912e1f4f12f240df8b
Zhang Yunjie <e0421414@u.nus.edu>
2021-04-11 22:05:10 +0800

    Modify Homepage - change icons when switching columns

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import Item from "../components/Item";
+import DropWrapper from "../components/DropWrapper";
+import Col from "../components/Col";
+import { data, statuses } from "../data";
 
-const Homepage = props => {
-    return (
-        <div>
-            Time to start coding!
-        </div>
-    );
+const Homepage = () => {
+    const [items, setItems] = useState(data);
+
+    const onDrop = (item, monitor, status) => {
+        const mapping = statuses.find(si => si.status === status);
+
+        setItems(prevState => {
+            const newItems = prevState
+                .filter(i => i.id !== item.id)
+                .concat({ ...item, status, icon: mapping.icon });
+            return [ ...newItems ];
+        });
+    };
+
+
 };
 
 export default Homepage;
