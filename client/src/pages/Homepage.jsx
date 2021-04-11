ad37bb5969a17eaf97abd745d3031eb1987003da
Zhang Yunjie <e0421414@u.nus.edu>
2021-04-11 22:11:28 +0800

    Modify Homepage. Show only items whose statuses match the column status for the columns.

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -16,6 +16,35 @@ const Homepage = () => {
                 .concat({ ...item, status, icon: mapping.icon });
             return [ ...newItems ];
         });
+
+        const moveItem = (dragIndex, hoverIndex) => {
+            const item = items[dragIndex];
+            setItems(prevState => {
+                const newItems = prevState.filter((i, idx) => idx !== dragIndex);
+                newItems.splice(hoverIndex, 0, item);
+                return  [ ...newItems ];
+            });
+        };
+    
+        return (
+            <div className={"row"}>
+                {statuses.map(s => {
+                    return (
+                        <div key={status} className={"col-wrapper"}>
+                            <h2 className={"col-header"}>{s.status.toUpperCase()}</h2>
+                            <DropWrapper onDrop={onDrop} status={s.status}>
+                                <Col>
+                                    {items
+                                        .filter(i => i.status === s.status)
+                                        .map((i, idx) => <Item key={i.id} item={i} index={idx} moveItem={moveItem} status={s} />)
+                                    }
+                                </Col>
+                            </DropWrapper>
+                        </div>
+                    );
+                })}
+            </div>
+        );
     };
 
 
